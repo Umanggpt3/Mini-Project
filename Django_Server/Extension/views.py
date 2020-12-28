86dcd1440ab61057d516e26d51192d5b43748320
Umang Gupta <umanggpt3@gmail.com>
2020-12-28 23:32:46 +0530

    Added screenshots

--- Django_Server/Extension/views.py
+++ Django_Server/Extension/views.py
@@ -14,9 +14,10 @@ from gensim.models.doc2vec import TaggedDocument, Doc2Vec
 
 @csrf_exempt
 def getTweet(request):
-    #Getting post
+    # Retrieving the post text and the selection filter
     tweet = request.POST.get('tweet')
     sortParam = request.POST.get('sortBy')
+    # By default its Relevancy
     if sortParam == 'undefined':
         sortParam = 'relevancy'
 
@@ -24,17 +25,23 @@ def getTweet(request):
     print(tweet)
     print(sortParam)
 
+    # Getting all the English language stopwords like and, the, is, etc
     stopwords_en = stopwords.words("english")
 
-    #Using NLTK here -
+    # Using NLTK here -
+    # Individual tokens or words are extracted from the text
     doc = nltk.word_tokenize(tweet)
+    # Stopwords are removed
     tweet_text = preprocessing(doc, stopwords_en)
+    # Parts of speech tagging is applied
     doc = nltk.pos_tag(doc)
     print(doc)
     print(tweet_text)
+    # Assigns the list of words a unique id or tag
     doc1 = TaggedDocument(words=tweet_text, tags=[u'NEWS_1'])
     taggeddocs.append(doc1)
 
+    # Extracting all types of nouns (simple, singular, plural, proper, etc)
     nnpkeywords = []
     nnskeywords = []
     nnkeywords = []
@@ -48,6 +55,7 @@ def getTweet(request):
             if (duo[1]) == 'NN':
                 nnkeywords.append(duo[0])
     
+    # Cannot take a lot of keywords for API call
     pos = len(nnpkeywords)
     if len(nnpkeywords) <= 2:
         for i in range(len(nnskeywords)): 
@@ -58,7 +66,7 @@ def getTweet(request):
         for i in range(len(nnkeywords)): 
             nnpkeywords.insert(i + pos, nnkeywords[i])
 
-    #News API calls
+    # News API calls
     api_key = '7f4ed22aa44b4226ab27a1cb363acf85'
     url = 'https://newsapi.org/v2/everything?'
 
@@ -74,7 +82,7 @@ def getTweet(request):
             'sortBy': sortParam,
         }
 
-        #Getting response 
+        # Calling the API and Getting a response 
         response = requests.get(url, params = parameters)
         r = response.json()
         if r['status'] != 'error':
@@ -83,7 +91,8 @@ def getTweet(request):
         
         if len(nnpkeywords) == 0:
             break
-            
+        
+        #if keywords are a lot
         nnpkeywords.pop()
 
     if r['status'] != 'error':
@@ -95,41 +104,54 @@ def getTweet(request):
 
             similar = []
             for i in range(6):
+                # Sometimes there can be no description
                 if result[i]['description'] == None:
                     wordlist = nltk.word_tokenize(result[i]['title'])
                 else:
                     wordlist = nltk.word_tokenize(result[i]['title'] + result[i]['description'])
+                # Removing stopwords
                 text = preprocessing(wordlist, stopwords_en)
                 print(text)
+                # Assigning words with a tag
                 doc2 = TaggedDocument(words=text, tags=[u'NEWS_2'])
                 taggeddocs.append(doc2)
 
-                #Build Model
+                # Build Model
+                # taggeddocs - Iterable list of TaggedDocs
+                # dm (0,1) is for training algorithm, 0 - Distributed Bag Of Words, 1 - Distributed Memory
+                # alpha - initial learning rate
+                # vector_size - dimentionality of the feature vector
+                # min_alpha - learning rate will linearly drop to min_alpha
+                # min_count - Ignores all words with total freq less than this 
                 model = Doc2Vec(taggeddocs, dm=0, alpha=0.025, vector_size=20, min_alpha=0.025, min_count=0)
 
-                #Train Model
-                for e in range(80):
+                # Training the Model
+                for _ in range(80):
                     model.train(taggeddocs, total_examples=model.corpus_count, epochs=model.iter)
                     model.alpha -= 0.002
                     model.min_alpha = model.alpha
 
+                # Finding the similarity
                 similarity = model.n_similarity(tweet_text, text)
                 print("Similarity Index: {:4.2f} %".format(similarity*100))
 
+                # Formatting
                 similar.append(format(similarity*100, '.0f'))
                 print(similar)
 
             r['similarity'] = similar
 
+            # Return proper articles with their similarity %s
             return JsonResponse(r, safe=True)
         else:
+            # Return 0 indicated error
             return HttpResponseNotFound('0')
     else:
+        # return 0 indicates error
         return HttpResponseNotFound('0')
 
 
 # Create your views here.
-
 @csrf_exempt
 def preprocessing(wordlist, stopwords):
     text = [w.lower() for w in wordlist if w not in stopwords]
--- Extension/background.js
+++ Extension/background.js
@@ -8,13 +8,17 @@ chrome.contextMenus.create({
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId == "select") {
+        //Selected post text
         let tweet = info.selectionText;
+        //Formatting the text
         let tweet_string = "tweet=" + tweet + "&sortBy=" + index;
+        //Http request
         let xhr = new XMLHttpRequest();
         let theUrl = "http://127.0.0.1:8000/Extension_data/";
         xhr.open("POST", theUrl, true);
         let r, res;
         xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");
+        //Sending a POST request
         xhr.send(tweet_string);
 
         xhr.onload = function() {
@@ -24,6 +28,7 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
                 this.abort();
             } else {
                 console.log(r);
+                //storing the news articles in the local storage
                 localStorage.sharedData = r;
                 r = JSON.parse(r);
                 if(r) {
@@ -31,10 +36,12 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
                     console.log(res);
                 }
                 let newTabURL = chrome.extension.getURL('print.html');
+                //open a new tab to display the relevant articles
                 chrome.tabs.create({url: newTabURL, active: true});
             }
         };
 
+        //errors like url not found
         xhr.onerror = function() {
             alert("Can't find relevant Articles.");
         }
